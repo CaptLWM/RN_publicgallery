4b9f52ca6b62a8935112351cf806634970a00a09
CaptLWM <wonmin275@gmail.com>
2022-08-25 22:42:56 +0900

    RN 8 firebase

---
+++ screens/RootStack.js
@@ -0,0 +1,10 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+
+const Stack = createNativeStackNavigator();
+
+const RootStack = () => {
+  return <Stack.Navigator>{/*화면 추가 예정 */}</Stack.Navigator>;
+};
+
+export default RootStack;
